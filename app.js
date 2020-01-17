da11af0a40aff6266bee4e9754b5aea2522e8727
Eli Zevin <dizzyjaguar@gmail.com>
2020-01-16 17:39:15 -0800

    elis changes

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 //initialize initial fetch state
-import { getRandomThrow } from './get-random-throw.js';
-import { checkResult } from './check-result.js';
+import getRandomThrow from './get-random-throw.js';
+import checkResult from './check-result.js';
 // grabing Elements for DOM
 const resultDisplay = document.getElementById('result-display');
-const myButton = document.getElementById('submit-guess');
-const resultImage = document.getElementById('result-image');
-const winsDisplay = document.getElementById('user-score');
-const lossesDisplay = document.getElementById('computer-score');
-const tiesDisplay = document.getElementById('ties');
+const myButton = document.getElementById('dorjes-button');
+const winsDisplay = document.getElementById('wins-display');
+const lossesDisplay = document.getElementById('losses-display');
+const tiesDisplay = document.getElementById('ties-display');
 // initial states
 let wins = 0;
 let losses = 0;
@@ -15,13 +14,14 @@ let ties = 0;
 // add event listeners
 //comparing the computer vs the user choice
 myButton.addEventListener('click', () =>{
-    const userInput = document.querySelector ('input:checked');
-    const userChoice = userInput.nodeValue;
-    const computerChoice = getRandomThrow;
-    const result = checkResult (userChoice, computerChoice);
-    console.log('user', userChoice);
-    console.log('computer', computerChoice);
+    const userInput = document.querySelector('input:checked');
+    const userChoice = userInput.value;
+    const computerChoice = getRandomThrow();
+    const result = checkResult(userChoice, computerChoice);
+
     console.log(result);
+    console.log(userChoice);
+    console.log(computerChoice);
 //here the TA's put in images for computer choice
     if (result === 'You win!'){
         wins++;
