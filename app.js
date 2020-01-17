44aead4e1154b7e838c38d1d139915dee39a1e9b
dorjepadma <dorjepadma@gmail.com>
2020-01-16 23:02:18 -0800

    late night debut

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //initialize initial fetch state
 import getRandomThrow from './get-random-throw.js';
 import checkResult from './check-result.js';
-//I am not clear onw how to integrate these two funcitons into the PerformanceNavigationTiming, thus they are not integrated yet.
+
 // grabing Elements for DOM
 const resultDisplay = document.getElementById('result-display');
 const myButton = document.getElementById('dorjes-button');
@@ -12,7 +12,6 @@ const tiesDisplay = document.getElementById('ties-display');
 let wins = 0;
 let losses = 0;
 let ties = 0;
-// add event listeners I believe I need two event listeners here. one for get random throw and one for check results...?
 //comparing the computer vs the user choice
 myButton.addEventListener('click', () =>{
     
@@ -20,23 +19,23 @@ myButton.addEventListener('click', () =>{
     const userChoice = userInput.value;
     const computerChoice = getRandomThrow();
     const result = checkResult(userChoice, computerChoice);
-    
+    console.log(result);
 //here the TA's put in images for computer choice
-    if (result === 'You win!'){
+    if (result === 'You Win!'){
         wins++;
         winsDisplay.textContent = wins;//update state
         resultDisplay.classList.remove('hidden');//update dom
-        resultDisplay.textContent = 'You win!';
+        resultDisplay.textContent = 'You Win!';
     } else if (result === 'Computer Wins!'){
         losses++;
         lossesDisplay.textContent = losses;
         resultDisplay.classList.remove('hidden');
         resultDisplay.textContent = 'Computer Wins!!';
-    } else if (result === 'tie!'){ 
+    } else if (result === 'Tie!'){
         ties++;
         tiesDisplay.textContent = ties;
         resultDisplay.classList.remove ('hidden');
         resultDisplay.textContent = 'Tie!';
     }
-    //maybe almost there again
+    
 });
--- get-random-throw.js
+++ get-random-throw.js
@@ -9,38 +9,4 @@ export default function getRandomThrow(){
     if (randomNumber === 3){
         return 'scissors';
     }
-    function checkResult(playerChoice, computerChoice) {
-        if (playerChoice === computerChoice){
-            console.log('Tie!');
-            return 'Tie!';
-        }
-        else if (playerChoice === 'paper' && computerChoice === 'rock')
-        {
-            console.log ('You win!');
-            return 'You win!';
-        } else if (playerChoice === 'rock' && computerChoice === 'scissors')
-        {
-            console.log ('You win!');
-            return 'You win!';
-        } else if (playerChoice === 'scissors' && computerChoice === 'paper')
-        {
-            console.log ('You win!');
-            return 'You win!';
-        } else if (playerChoice === 'paper' && computerChoice === 'scissors')
-        {
-            console.log ('Computer Wins!');
-            return 'Computer Wins!';
-        } else if (playerChoice === 'rock' && computerChoice === 'paper')
-        {
-            console.log ('Computer Wins!');
-            return 'Computer Wins!';
-        } else (playerChoice === 'scissors' && computerChoice === 'rock');
-        {
-            console.log ('Computer Wins!');
-            return 'Computer Wins!';
-        }
-    }
-    
-    
-    
 }
